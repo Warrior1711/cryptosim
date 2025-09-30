939cd02601a475ca40a4c18a2a526f245f178c82
Losercities <133237995+Warrior1711@users.noreply.github.com>
2025-09-30 12:11:58 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,15 +2,15 @@ export const coins = [
   {
     symbol: 'BTC',
     name: 'Bitcoin',
-    price: 27000,
-    circulation: 21000000, // Editable in-game
+    price: 7600,
+    circulation: 550, // Editable in-game
     editable: true,
   },
   {
     symbol: 'LTC',
     name: 'Litecoin',
     price: 80,
-    circulation: 84000000, // Editable in-game
+    circulation: 1500, // Editable in-game
     editable: true,
   }
 ];
